22a2052b0dc89782d7ad250b75e021e8b36c1bb3
Anton Ivanov <ivanovdevelop@yandex.ru>
2022-06-22 17:28:36 +0300

    delete images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,17 +1,5 @@
 import logo from './logo/logo.svg'
-import profilePic from './images/working.jpg'
-import arrow from './images/arrow.svg'
-import med from './images/works/med.png'
-import pizza from './images/works/pizza.png'
-import snow from './images/works/snow.png'
-import fam from './images/works/fam.png'
 
 export {
     logo,
-    profilePic,
-    arrow,
-    med, 
-    pizza,
-    snow,
-    fam
 }
